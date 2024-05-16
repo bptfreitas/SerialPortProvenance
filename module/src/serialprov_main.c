fdc22134a8f3e66e4fc08a0622cb5a9a8aaada9e
Bruno <bruno.freitas@cefet-rj.br>
2024-05-16 17:44:40 -0300

    Code cleanup

--- module/src/serialprov_main.c
+++ module/src/serialprov_main.c
@@ -57,267 +57,13 @@
 #define DRIVER_AUTHOR "Bruno Policarpo <bruno.freitas@cefet-rj.br>"
 #define DRIVER_DESC "SerialProv Driver"
 
-#define DELAY_TIME 2
+#define DELAY_TIME 2*HZ
 
 /* Module information */
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
 
-
-
-#if 0
-static int __init virtualbot_init(void)
-{
-	int retval;
-	unsigned i;
-
-	/* allocate the tty driver */
-	//virtualbot_tty_driver = alloc_tty_driver(virtualbot_TTY_MINORS);
-
-	/*
-	 * Initializing the VirtialBot driver
-	 * 
-	 * This is the main hardware emulation device
-	 * 
-	*/
-
-	virtualbot_tty_driver = tty_alloc_driver( VIRTUALBOT_MAX_TTY_MINORS, \
-		TTY_DRIVER_REAL_RAW );
-
-	if (!virtualbot_tty_driver)
-		return -ENOMEM;
-
-	/* initialize the tty driver */
-	virtualbot_tty_driver->owner = THIS_MODULE;
-	virtualbot_tty_driver->driver_name = VIRTUALBOT_DRIVER_NAME;
-	virtualbot_tty_driver->name = VIRTUALBOT_TTY_NAME;
-	virtualbot_tty_driver->major = VIRTUALBOT_TTY_MAJOR,
-	virtualbot_tty_driver->minor_start = 0,
-	virtualbot_tty_driver->type = TTY_DRIVER_TYPE_SERIAL,
-	virtualbot_tty_driver->subtype = SERIAL_TYPE_NORMAL,
-	virtualbot_tty_driver->flags = TTY_DRIVER_RESET_TERMIOS | TTY_DRIVER_REAL_RAW | TTY_DRIVER_DYNAMIC_DEV,
-	virtualbot_tty_driver->init_termios = tty_std_termios;
-	virtualbot_tty_driver->init_termios.c_cflag = B9600 | CS8 | CREAD | HUPCL | CLOCAL;
-	virtualbot_tty_driver->init_termios.c_iflag = 0;
-	virtualbot_tty_driver->init_termios.c_oflag = 0;
-	
-	// Must be 0 to disable ECHO flag
-	virtualbot_tty_driver->init_termios.c_lflag = 0;
-	
-	//virtualbot_tty_driver->init_termios = tty_std_termios;
-	//virtualbot_tty_driver->init_termios.c_cflag = B38400 | CS8 | CREAD;
-	//virtualbot_tty_driver->init_termios.c_lflag = 0;
-	//virtualbot_tty_driver->init_termios.c_ispeed = 38400;
-	//virtualbot_tty_driver->init_termios.c_ospeed = 38400;
-
-	tty_set_operations(virtualbot_tty_driver, 
-		&virtualbot_serial_ops);
-
-	pr_debug("virtualbot: set operations");
-
-	for (i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; i++) {
-
-		tty_port_init( &virtualbot_tty_port[ i ]);
-		pr_debug("virtualbot: port %i initiliazed", i);
-
-		tty_port_register_device( &virtualbot_tty_port[ i ], 
-			virtualbot_tty_driver, 
-			i, 
-			NULL);
-		pr_debug("virtualbot: port %i linked", i);
-
-		virtualbot_table[ i ] = NULL;
-		
-		mutex_init( &virtualbot_lock[ i ] );
-	}
-
-	/* register the tty driver */
-	retval = tty_register_driver(virtualbot_tty_driver);
-
-	if (retval) {
-		pr_err("virtualbot: failed to register virtualbot tty driver");
-
-		tty_driver_kref_put(virtualbot_tty_driver);
-
-		return retval;
-	}
-	
-	//pr_info("virtualbot: driver initialized (" DRIVER_DESC " " DRIVER_VERSION  ")" );
-
-	/*
-	 * 
-	 * Initializing the VirtualBot Commander
-	 * 
-	 * This is the main interface between
-	 * 
-	*/
-
-	vb_comm_tty_driver = tty_alloc_driver( VIRTUALBOT_MAX_TTY_MINORS,
-		TTY_DRIVER_REAL_RAW );
-
-	if (!vb_comm_tty_driver){
-		retval = -ENOMEM;
-	}
-
-	/* initialize the driver */
-	vb_comm_tty_driver->owner = THIS_MODULE;
-	vb_comm_tty_driver->driver_name = VB_COMM_DRIVER_NAME;
-	vb_comm_tty_driver->name = VB_COMM_TTY_NAME;
-	vb_comm_tty_driver->major = VB_COMM_TTY_MAJOR,
-	vb_comm_tty_driver->minor_start = 0,
-	vb_comm_tty_driver->type = TTY_DRIVER_TYPE_SERIAL,
-	vb_comm_tty_driver->subtype = SERIAL_TYPE_NORMAL,
-	vb_comm_tty_driver->flags = TTY_DRIVER_RESET_TERMIOS | TTY_DRIVER_REAL_RAW | TTY_DRIVER_DYNAMIC_DEV,
-	vb_comm_tty_driver->init_termios = tty_std_termios;
-	vb_comm_tty_driver->init_termios.c_cflag = B9600 | CS8 | CREAD | HUPCL | CLOCAL;
-	vb_comm_tty_driver->init_termios.c_iflag = 0;
-	vb_comm_tty_driver->init_termios.c_oflag = 0;	
-	
-	// Must be 0 to disable ECHO flag
-	vb_comm_tty_driver->init_termios.c_lflag = 0;	
-
-	tty_set_operations(vb_comm_tty_driver, 
-		&vb_comm_serial_ops);
-
-	pr_debug("vb-comm: set operations");
-
-	for (i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; i++) {
-
-		tty_port_init(& vb_comm_tty_port[ i ] );
-		pr_debug("vb-comm: port %i initiliazed", i);
-
-		tty_port_register_device( & vb_comm_tty_port[ i ], 
-			vb_comm_tty_driver, 
-			i, 
-			NULL);
-		pr_debug("vb-comm: port %i linked", i);
-
-		// Port state is initially NULL
-		vb_comm_table[ i ] = NULL;
-		
-		mutex_init( &vb_comm_lock[ i ] );
-	}
-
-
-	/* register the tty driver */
-	retval = tty_register_driver(vb_comm_tty_driver);
-
-	if (retval) {
-
-		pr_err("vb-comm: failed to register vb-comm tty driver");
-
-		tty_driver_kref_put(vb_comm_tty_driver);
-
-		return retval;
-	}
-
-	for ( i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; i++ ){
-		mutex_init( &virtualbot_global_port_lock[ i ] );
-	}
-
-	pr_info("Serial Port Emulator initialized (" DRIVER_DESC " " DRIVER_VERSION  ")" );
-
-	return retval;
-}
-
-static void __exit virtualbot_exit(void)
-{
-	struct virtualbot_serial *virtualbot;
-	struct vb_comm_serial *vb_comm;
-	int i;
-
-	// struct list_head *pos, *n;
-
-	for (i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; ++i) {
-		
-		tty_unregister_device(virtualbot_tty_driver, i);
-		
-		pr_debug("virtualbot: device %d unregistered" , i);
-
-		tty_port_destroy(virtualbot_tty_port + i);
-
-		pr_debug("virtualbot: port %i destroyed", i);
-	}
-
-	tty_unregister_driver(virtualbot_tty_driver);
-
-	tty_driver_kref_put(virtualbot_tty_driver);
-
-	pr_debug("virtualbot: driver unregistered");
-
-	/* shut down all of the timers and free the memory */
-	for (i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; i++) {
-
-		virtualbot = virtualbot_table[i];
-
-		pr_debug("virtualbot: freeing VB %i", i);
-
-		if (virtualbot) {
-			/* close the port */
-			while (virtualbot->open_count)
-				do_close(virtualbot);
-
-			/* shut down our timer and free the memory */
-			del_timer(&virtualbot->timer);
-
-			/* deallocate receive buffer */
-			kfree( virtualbot->recv_buffer.buf );
-			
-			/* destroy structure mutex */
-			mutex_destroy( &virtualbot_lock[ i ] );
-			
-			kfree(virtualbot);
-			virtualbot_table[i] = NULL;
-		}
-
-	}
-
-	/**
-	 * 
-	 * Unregistering The Comm part 
-	 * 
-	*/
-
-	for (i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; ++i) {
-
-		tty_unregister_device(vb_comm_tty_driver, i);
-		
-		pr_debug("vb-comm: device %d unregistered" , i);
-
-		tty_port_destroy(vb_comm_tty_port + i);
-
-		pr_debug("vb-comm: port %i destroyed", i);
-	}
-
-	tty_unregister_driver(vb_comm_tty_driver);
-
-	tty_driver_kref_put(vb_comm_tty_driver);
-
-	// For future
-	for (i = 0; i < VIRTUALBOT_MAX_TTY_MINORS; i++) {
-
-		vb_comm = vb_comm_table[i];
-
-		pr_debug("vb-comm: freeing VB %i", i);
-
-		if (vb_comm) {
-			/* close thvirtualbote port */
-			while (vb_comm->open_count)
-				vb_comm_do_close(vb_comm);
-				
-			mutex_destroy( &vb_comm_lock[ i ] );
-
-			/* shut down our timer and free the memory */
-			//del_timer(&virtualbot->timer);
-			kfree(vb_comm);
-			vb_comm_table[i] = NULL;
-		}
-	}
-
-	mutex_destroy( &virtualbot_global_port_lock[ i ] );
-}
-#endif
 
 static struct tty_struct *tty_struct_to_listen;
 
@@ -329,6 +75,20 @@ struct timer_list listener_timer;
 static void tty_listener(struct timer_list *t)
 {
 	pr_debug("serialprov: running listener");
+	
+	tty_port_to_listen = tty_struct_to_listen->port;
+	
+	if (tty_port_to_listen == NULL){
+	
+		// Pointer is null, port is not open - abort
+		
+		pr_err("serialprov: error loading tty_port!");
+		
+	} else {
+	
+		// Pointer is valid, get data
+	
+	}
 
 	/* resubmit the timer again */		
 	timer_setup(t, tty_listener, 0);
@@ -375,17 +135,6 @@ static int __init serialprov_init(void)
 		goto ret_error;		
 	}
 	
-	tty_port_to_listen = tty_struct_to_listen->port;
-	
-	if (tty_port_to_listen == NULL){
-	
-		retval = -1;
-	
-		pr_err("serialprov: error loading tty_port!");
-		
-		goto free_tty_struct;		
-	}	
-	
 	timer_setup(&listener_timer, tty_listener, 0);
 
 	listener_timer.expires = jiffies + DELAY_TIME;
@@ -422,7 +171,7 @@ static void __exit serialprov_exit(void)
 	
 	if (retval != 0 ){
 	
-		pr_err("Error deleting timer!");
+		pr_info("Active tty_listener deleted");
 	
 	}
 	
